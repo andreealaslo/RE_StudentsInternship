1f09231fa8b0c5ccf28d3327d837d0ce6706cf68
AlexandraaLupu <alexlupu973@gmail.com>
2025-01-04 20:55:38 +0200

    Add internship entity and implement get & create & search

---
+++ backend/src/main/java/com/example/assignment_3/model/Internship.java
@@ -0,0 +1,34 @@
+package com.example.assignment_3.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "Internships")
+@Data
+public class Internship {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "company_id", nullable = false)
+    private Company company;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false, length = 1000)
+    private String description;
+
+    @Column(nullable = false)
+    private String location;
+
+    @Column(nullable = false)
+    private String duration;
+
+    @Column(nullable = false)
+    private String requirements;
+
+}
